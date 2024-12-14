d605b4d75a7d73ab7991b28cf50156703156f8d9
suif4599 <2673643436@qq.com>
2024-12-14 10:17:52 +0800

    all

---
+++ c_works/source/simple_tcp_connect.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#endif
+#ifdef __linux__
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#endif
