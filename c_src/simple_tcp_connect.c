a7562c955337b300ebed9029a4dd069526af6033
suif4599 <2673643436@qq.com>
2024-12-14 10:53:42 +0800

    all

--- c_src/simple_tcp_connect.c
+++ c_src/simple_tcp_connect.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #ifdef _WIN32
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #endif
+
 #ifdef __linux__
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #endif
+
+#include "c_include/simple_tcp_connect.h"
+
+void test() {
+    printf("Hello, World!\n");
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "c_include/simple_tcp_connect.h"
+
+int main() {
+    test();
+    return 0;
+}
